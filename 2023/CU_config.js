9dfea8c2f817e4826c488e525a6fc3b89a0626e6
Jaycoder7 <68451905+Jaycoder7@users.noreply.github.com>
2023-01-16 07:12:27 -0500

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -1,209 +1,209 @@
-"var config_data =""`
+var config_data = `
 {
-  \"title\": \"Scouting App 2023\",
-  \"page_title\": \"Charged Up\",
-  \"checkboxAs\": \"10\",
-  \"prematch\": [
-    { \"name\": \"Scouter Initials\",
-      \"code\": \"s\",
-      \"type\": \"scouter\",
-      \"size\": 5,
-      \"maxSize\": 5,
-      \"required\": \"true\"
+  "title": "Scouting App 2023",
+  "page_title": "Charged Up",
+  "checkboxAs": "10",
+  "prematch": [
+    { "name": "Scouter Initials",
+      "code": "s",
+      "type": "scouter",
+      "size": 5,
+      "maxSize": 5,
+      "required": "true"
     },
-    { \"name\": \"Event\",
-      \"code\": \"e\",
-      \"type\": \"event\",
-      \"defaultValue\": \"2022flwp\",
-      \"required\": \"true\",
-      \"disabled\": \"false\"
+    { "name": "Event",
+      "code": "e",
+      "type": "event",
+      "defaultValue": "2022flwp",
+      "required": "true",
+      "disabled": "false"
     },
-    { \"name\": \"Pit #\",
-      \"code\": \"m\",
-      \"type\": \"match\",
-      \"min\": 1,
-      \"max\": 100,
-      \"required\": \"true\"
+    { "name": "Pit #",
+      "code": "m",
+      "type": "match",
+      "min": 1,
+      "max": 100,
+      "required": "true"
     },
-    { \"name\": \"Team #\",
-      \"code\": \"t\",
-      \"type\": \"team\",
-      \"min\": 1,
+    { "name": "Team #",
+      "code": "t",
+      "type": "team",
+      "min": 1,
     }
   ],
-  \"auton\": [
-    { \"name\": \"Mobility?\",
-      \"code\": \"am\",
-      \"type\": \"bool\"
+  "auton": [
+    { "name": "Mobility?",
+      "code": "am",
+      "type": "bool"
     },
-    { \"name\": \"High Cube Scored\",
-      \"code\": \"auh\",
-      \"type\": \"counter\"
+    { "name": "High Cube Scored",
+      "code": "auh",
+      "type": "counter"
     },
-    { \"name\": \"Medium Cube Scored\",
-      \"code\": \"aum\",
-      \"type\": \"counter\"
+    { "name": "Medium Cube Scored",
+      "code": "aum",
+      "type": "counter"
     },
-    { \"name\": \"Low Cube Scored\",
-      \"code\": \"aul\",
-      \"type\": \"counter\"
+    { "name": "Low Cube Scored",
+      "code": "aul",
+      "type": "counter"
     },
-    { \"name\": \"High Cone Scored\",
-      \"code\": \"aoh\",
-      \"type\": \"counter\"
+    { "name": "High Cone Scored",
+      "code": "aoh",
+      "type": "counter"
     },
-    { \"name\": \"Medium Cone Scored\",
-      \"code\": \"aom\",
-      \"type\": \"counter\"
+    { "name": "Medium Cone Scored",
+      "code": "aom",
+      "type": "counter"
     },
-    { \"name\": \"Low Cone Scored\",
-      \"code\": \"aol\",
-      \"type\": \"counter\"
+    { "name": "Low Cone Scored",
+      "code": "aol",
+      "type": "counter"
     },
-    { \"name\": \"Docked\",
-      \"code\": \"ad\",
-      \"type\":\"radio\",
-      \"choices\": {
-        \"docked\": \"Docked (not Engaged)<br>\",
-        \"engaged\": \"Engaged<br>\",
-        \"attempted but failed\": \"Attempted but failed<br>\",
-        \"Not Attemmpted\": \"Not attempted\"
+    { "name": "Docked",
+      "code": "ad",
+      "type":"radio",
+      "choices": {
+        "docked": "Docked (not Engaged)<br>",
+        "engaged": "Engaged<br>",
+        "attempted but failed": "Attempted but failed<br>",
+        "Not Attemmpted": "Not attempted"
       },
-      \"defaultValue\": \"docked\"
+      "defaultValue": "docked"
     }
   ],
-  \"teleop\": [
-    { \"name\": \"Cycle Timer(seconds)\",
-      \"code\": \"tct\",
-      \"type\": \"counter\"
+  "teleop": [
+    { "name": "Cycle Timer(seconds)",
+      "code": "tct",
+      "type": "counter"
     },
-    { \"name\": \"High Cube Scored\",
-      \"code\": \"tuh\",
-      \"type\": \"counter\"
+    { "name": "High Cube Scored",
+      "code": "tuh",
+      "type": "counter"
     },
-    { \"name\": \"Medium Cube Scored\",
-      \"code\": \"tum\",
-      \"type\": \"counter\"
+    { "name": "Medium Cube Scored",
+      "code": "tum",
+      "type": "counter"
     },
-    { \"name\": \"Low Cube Scored\",
-      \"code\": \"tul\",
-      \"type\": \"counter\"
+    { "name": "Low Cube Scored",
+      "code": "tul",
+      "type": "counter"
     },
-    { \"name\": \"High Cone Scored\",
-      \"code\": \"toh\",
-      \"type\": \"counter\"
+    { "name": "High Cone Scored",
+      "code": "toh",
+      "type": "counter"
     },
-    { \"name\": \"Medium Cone Scored\",
-      \"code\": \"tom\",
-      \"type\": \"counter\"
+    { "name": "Medium Cone Scored",
+      "code": "tom",
+      "type": "counter"
     },
-    { \"name\": \"Low Cone Scored\",
-      \"code\": \"tol\",
-      \"type\": \"counter\"
+    { "name": "Low Cone Scored",
+      "code": "tol",
+      "type": "counter"
     },
-    { \"name\": \"Feeder Count\",
-      \"code\": \"tfc\",
-      \"type\": \"counter\"
+    { "name": "Feeder Count",
+      "code": "tfc",
+      "type": "counter"
     },
-    { \"name\": \"Smart Placement (creates Links)\",
-      \"code\": \"lnk\",
-      \"type\": \"bool\"
+    { "name": "Smart Placement (creates Links)",
+      "code": "lnk",
+      "type": "bool"
     },
-    { \"name\": \"Cargo Intake From\",
-      \"code\": \"cif\",
-      \"type\": \"radio\",
-      \"choices\": {
-        \"Substation\": \"Substation<br>\",
-        \"ground\": \"Ground<br>\",
-        \"both\": \"Both<br>\",
-        \"not attempted\": \"Not Attempted\"
+    { "name": "Cargo Intake From",
+      "code": "cif",
+      "type": "radio",
+      "choices": {
+        "Substation": "Substation<br>",
+        "ground": "Ground<br>",
+        "both": "Both<br>",
+        "not attempted": "Not Attempted"
       },
-      \"defaultValue\": \"both\"
+      "defaultValue": "both"
     }
   ],
-  \"endgame\": [
-    { \"name\": \"Docking Timer(seconds)\",
-      \"code\": \"dt\",
-      \"type\": \"counter\"
+  "endgame": [
+    { "name": "Docking Timer(seconds)",
+      "code": "dt",
+      "type": "counter"
     },
-    { \"name\": \"Final Status\",
-      \"code\": \"fs\",
-      \"type\":\"radio\",
-      \"choices\": {
-        \"parked\": \"Parked<br>\",
-        \"docked(not engaged)\": \"Docked (Not Engaged)<br>\",
-        \"engaged\": \"Engaged<br>\",
-        \"attempted but failed\": \"Attempted but failed<br>\",
-        \"not attempted\": \"Not attempted\"
+    { "name": "Final Status",
+      "code": "fs",
+      "type":"radio",
+      "choices": {
+        "parked": "Parked<br>",
+        "docked(not engaged)": "Docked (Not Engaged)<br>",
+        "engaged": "Engaged<br>",
+        "attempted but failed": "Attempted but failed<br>",
+        "not attempted": "Not attempted"
       },
-      \"defaultValue\": \"parked\"
+      "defaultValue": "parked"
     }
   ],
-  \"postmatch\": [
-    { \"name\": \"Driver Skill\",
-      \"code\": \"ds\",
-      \"type\": \"radio\",
-      \"choices\": {
-        \"not effective\": \"Not Effective<br>\",
-        \"average\": \"Average<br>\",
-        \"very effective\": \"Very Effective<br>\",
-        \"not observed\": \"Not Observed\"
+  "postmatch": [
+    { "name": "Driver Skill",
+      "code": "ds",
+      "type": "radio",
+      "choices": {
+        "not effective": "Not Effective<br>",
+        "average": "Average<br>",
+        "very effective": "Very Effective<br>",
+        "not observed": "Not Observed"
       },
-      \"defaultValue\": \"average\"
+      "defaultValue": "average"
     },
-    { \"name\": \"Links Scored\",
-      \"code\": \"ls\",
-      \"type\": \"counter\"
+    { "name": "Links Scored",
+      "code": "ls",
+      "type": "counter"
     },
-    { \"name\": \"Defense Rating\",
-      \"code\": \"dr\",
-      \"type\": \"radio\",
-      \"choices\": {
-        \"below average\": \"Below Average<br>\",
-        \"average\": \"Average<br>\",
-        \"good\": \"Good<br>\",
-        \"excellent\": \"Excellent<br>\",
-        \"did not play defense\": \"Did not play defense\"
+    { "name": "Defense Rating",
+      "code": "dr",
+      "type": "radio",
+      "choices": {
+        "below average": "Below Average<br>",
+        "average": "Average<br>",
+        "good": "Good<br>",
+        "excellent": "Excellent<br>",
+        "did not play defense": "Did not play defense"
       },
-      \"defaultValue\": \"good\"
+      "defaultValue": "good"
     },
-    { \"name\": \"Swerve drive?\",
-      \"code\": \"sd\",
-      \"type\": \"bool\"
+    { "name": "Swerve drive?",
+      "code": "sd",
+      "type": "bool"
     },
-    { \"name\": \"Speed Rating\",
-      \"code\": \"sr\",
-      \"type\": \"radio\",
-      \"choices\": {
-        \"1\": \"1 (slow)<br>\",
-        \"2\": \"2<br>\",
-        \"3\": \"3<br>\",
-        \"4\": \"4<br>\",
-        \"5\": \"5 (fast)\"
+    { "name": "Speed Rating",
+      "code": "sr",
+      "type": "radio",
+      "choices": {
+        "1": "1 (slow)<br>",
+        "2": "2<br>",
+        "3": "3<br>",
+        "4": "4<br>",
+        "5": "5 (fast)"
       },
-      \"defaultValue\":\"3\"
+      "defaultValue":"3"
     },
    
-    { \"name\": \"Make good alliance partner?\",
-      \"tooltip\": \"Would you want this robot on your alliance in eliminations?\",
-      \"code\": \"all\",
-      \"type\": \"bool\"
+    { "name": "Make good alliance partner?",
+      "tooltip": "Would you want this robot on your alliance in eliminations?",
+      "code": "all",
+      "type": "bool"
     },
-    { \"name\": \"Comments\",
-      \"code\": \"co\",
-      \"type\": \"text\",
-      \"size\": 15,
-      \"maxSize\": 50
+    { "name": "Comments",
+      "code": "co",
+      "type": "text",
+      "size": 15,
+      "maxSize": 50
     },
-    { \"name\": \"Confidence Rating\",
-      \"code\": \"cnf\",
-      \"type\": \"radio\",
-      \"choices\": {
-        \"very confident\": \"Very Confident<br>\",
-        \"average\": \"Average<br>\",
-        \"not confident\": \"Not Confident\"
+    { "name": "Confidence Rating",
+      "code": "cnf",
+      "type": "radio",
+      "choices": {
+        "very confident": "Very Confident<br>",
+        "average": "Average<br>",
+        "not confident": "Not Confident"
         },
-      \"defaultValue\": \"average\"
+      "defaultValue": "average"
     }
   ]
-}`"";"
+}`;
